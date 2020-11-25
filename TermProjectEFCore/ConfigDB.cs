0bb8c499b0d1fc778eabb2861ea183bea0e6ec1b
Ruslan Kalashnikov <ruslan.avtomatov@gmail.com>
2020-11-24 18:02:17 -0800

    Added EFCore classes generated from the DB

---
+++ TermProjectEFCore/ConfigDB.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.Extensions.Configuration;
+
+namespace TermProjectEFCore
+{
+    public class ConfigDB
+    {
+        public static string GetMySqlConnectionString()
+        {
+            string folder = System.AppContext.BaseDirectory;
+            var builder = new ConfigurationBuilder()
+                    .SetBasePath(folder)
+                    .AddJsonFile("mySqlSettings.json", optional: true, reloadOnChange: true);
+
+            string connectionString = builder.Build().GetConnectionString("mySql");
+
+            return connectionString;
+        }
+    }
+}
