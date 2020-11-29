c0bdba93d17ce67f59da52b2789984f280939dff
Ruslan Kalashnikov <ruslan.avtomatov@gmail.com>
2020-11-28 22:44:16 -0800

    Finished regular postman tests

---
+++ CS234N-BITS/bitsEFClasses/Models/SupplierVM.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace bitsEFClasses.Models
+{
+    public class SupplierVM
+    {
+        public int SupplierId { get; set; }
+        public string Name { get; set; }
+        public string StrLine1 { get; set; }
+        public string StrLine2 { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+        public string Zip { get; set; }
+        public string Country { get; set; }
+    }
+}
