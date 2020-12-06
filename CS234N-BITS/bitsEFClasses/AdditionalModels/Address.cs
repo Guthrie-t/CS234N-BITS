489fddb30abe17432d5ceadf6282bf27e3112946
Ruslan Kalashnikov <ruslan.avtomatov@gmail.com>
2020-12-06 14:38:34 -0800

    Added model folder and classes provided by Mari

---
+++ CS234N-BITS/bitsEFClasses/AdditionalModels/Address.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace bitsEFClasses.AdditionalModels
+{
+    public class Address
+    {
+        public Address()
+        {
+        }
+        public Address(int addressId, string streetLine1, string streetLine2, string city, string state, string zip, int typeId, string typeName)
+        {
+            AddressId = addressId;
+            StreetLine1 = streetLine1;
+            StreetLine2 = streetLine2;
+            City = city;
+            State = state;
+            Zipcode = zip;
+            TypeId = typeId;
+            TypeName = typeName;
+        }
+
+        public int AddressId { get; set; }
+        public string StreetLine1 { get; set; }
+        public string StreetLine2 { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+        public string Zipcode { get; set; }
+        public int TypeId { get; set; }
+        public string TypeName { get; set; }
+    }
+}
---
+++ CS234N-BITS/bitsEFClasses/AdditionalModels/Supplier.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.EntityFrameworkCore;
+
+using bitsEFClasses.Models;
+using SimpleSupplier = bitsEFClasses.Models.Supplier;
+using SimpleAddress = bitsEFClasses.Models.Address;
+using System.Threading.Tasks;
+
+namespace bitsEFClasses.AdditionalModels
+{
+    public class Supplier
+    {
+        public Supplier()
+        {
+            Addresses = new List<Address>();
+            AddressTypes = new List<AddressType>();
+        }
+
+        public int SupplierId { get; set; }
+        public string Name { get; set; }
+        public string Phone { get; set; }
+        public string Email { get; set; }
+        public string ContactFirstName { get; set; }
+        public string ContactLastName { get; set; }
+        public string ContactPhone { get; set; }
+        public string ContactEmail { get; set; }
+        public string Note { get; set; }
+        public virtual ICollection<Address> Addresses { get; set; }
+        public virtual ICollection<AddressType> AddressTypes { get; set; }
+
+        public async Task FillSupplier(bitsContext dbContext, int supplierId)
+        {
+            SimpleSupplier s = await dbContext.Supplier.Include("SupplierAddress").Where(s => s.SupplierId == supplierId).FirstOrDefaultAsync();
+            SupplierId = s.SupplierId;
+            Name = s.Name;
+            Phone = s.Phone;
+            Email = s.Email;
+            ContactFirstName = s.ContactFirstName;
+            ContactLastName = s.ContactLastName;
+            ContactPhone = s.ContactPhone;
+            Note = s.Note;
+
+            foreach (SupplierAddress sAddress in s.SupplierAddress)
+            {
+                int addressId = sAddress.AddressId;
+                int addressTypeId = sAddress.AddressTypeId;
+                SimpleAddress address = await dbContext.Address.FindAsync(addressId);
+                AddressType aType = await dbContext.AddressType.FindAsync(addressTypeId);
+                Addresses.Add(new Address(addressId, address.StreetLine1, address.StreetLine2, address.City,
+                    address.State, address.Zipcode, addressTypeId, aType.Name));
+            }
+        }
+    }
+}
